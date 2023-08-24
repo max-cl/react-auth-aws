07c91f98aaf22c631f36de27a5f27ec7c44804c2
max-cl <maximiliano.arias@gmail.com>
2023-08-24 12:45:03 +0200

    Turn from main to section html tag

--- src/components/common/ContainerPage/index.tsx
+++ src/components/common/ContainerPage/index.tsx
@@ -3,5 +3,5 @@ interface Props {
 }
 
 export default function ContainerPage({ children }: Props) {
-    return <main className="h-[calc(100dvh_-_80px)] flex items-center justify-center">{children}</main>;
+    return <section className="h-[calc(100dvh_-_80px)] flex items-center justify-center">{children}</section>;
 }
